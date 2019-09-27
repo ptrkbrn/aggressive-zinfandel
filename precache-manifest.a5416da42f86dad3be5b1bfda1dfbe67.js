727d00b7f2ffca70fbd1f3d59149efc3782b1575
Patrick Breen <pressure78@gmail.com>
2019-09-26 22:56:56 -0400

    Updates

---
+++ precache-manifest.a5416da42f86dad3be5b1bfda1dfbe67.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4764e12ff130f36f054af7c15e4599a6",
+    "url": "/aggressive-zinfandel/index.html"
+  },
+  {
+    "revision": "dad5564d9b6a1ed1327e",
+    "url": "/aggressive-zinfandel/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "32fc0a92d059da98ce69",
+    "url": "/aggressive-zinfandel/static/css/main.6bca2775.chunk.css"
+  },
+  {
+    "revision": "dad5564d9b6a1ed1327e",
+    "url": "/aggressive-zinfandel/static/js/2.51a65aa1.chunk.js"
+  },
+  {
+    "revision": "32fc0a92d059da98ce69",
+    "url": "/aggressive-zinfandel/static/js/main.040c0278.chunk.js"
+  },
+  {
+    "revision": "4b2d65ccbd01446b6eac",
+    "url": "/aggressive-zinfandel/static/js/runtime-main.195a1faa.js"
+  },
+  {
+    "revision": "d3d1e58901ce7e52f79dc7032487f8f0",
+    "url": "/aggressive-zinfandel/static/media/intro1.d3d1e589.mp3"
+  },
+  {
+    "revision": "e3fc44620b3066dbc7b8e7e96415759e",
+    "url": "/aggressive-zinfandel/static/media/intro2.e3fc4462.mp3"
+  },
+  {
+    "revision": "80e36ee8d68462add856a039cbc23920",
+    "url": "/aggressive-zinfandel/static/media/intro3.80e36ee8.mp3"
+  },
+  {
+    "revision": "027248ae6e97100901d130b60efbc78c",
+    "url": "/aggressive-zinfandel/static/media/intro4.027248ae.mp3"
+  },
+  {
+    "revision": "881c465d5a9b82d17a272e01452df6e0",
+    "url": "/aggressive-zinfandel/static/media/intro5.881c465d.mp3"
+  }
+]);
